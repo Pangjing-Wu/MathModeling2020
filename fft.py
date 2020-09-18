f12b2e16d71b4585915620212e36ae7ca5377812
Pangjing-Wu <pangjing.wu@outlook.com>
2020-09-18 09:55:47 -0400

    update fft

--- fft.py
+++ fft.py
@@ -14,26 +14,25 @@ for i in range(3):
 
     dft = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
     fshift = np.fft.fftshift(dft)
-    shift2center = np.fft.fftshift(dft)
-    log_fft2 = np.log(1 + np.abs(dft))
-    ax[1,i].imshow(log_fft2[:,:,0], 'Blues')
+    log_fft2 = np.log(cv2.magnitude(dft[:,:,0], dft[:,:,1]))
+    ax[1,i].imshow(log_fft2, 'Blues')
     ax[1,i].set_title('log_fft2')
 
-    log_shift2center = np.log(1 + np.abs(shift2center))
-    ax[2,i].imshow(log_shift2center[:,:,0], 'Blues')
+    shift2center = np.fft.fftshift(dft)
+    log_shift2center = np.log(cv2.magnitude(shift2center[:,:,0], shift2center[:,:,1]))
+    ax[2,i].imshow(log_shift2center, 'Blues')
     ax[2,i].set_title('log shift spectrum')
 
     width = 30
     rows, cols = img.shape
-    crow,ccol = int(rows/2), int(cols/2) #中心位置
+    crow,ccol = int(rows/2), int(cols/2)
     mask = np.ones((rows, cols, 2), np.uint8)
     mask[crow-width:crow+width, ccol-width:ccol+width] = 0
-    #在频域上显示滤波器位置
-    mask_img = log_shift2center
-    mask_img[crow-width:crow+width, ccol-width:ccol+width] = 0
-    ax[3,i].imshow(mask_img[:,:,0], 'Blues')
-    ax[3,i].set_title('filter shifted spectrum')
 
+    mask_img = log_shift2center.copy()
+    mask_img[crow-width:crow+width, ccol-width:ccol+width] = 0
+    ax[3,i].imshow(mask_img, 'Blues')
+    ax[3,i].set_title('filter shifted spectrum')
 
     f = fshift * mask
     ishift = np.fft.ifftshift(f)
