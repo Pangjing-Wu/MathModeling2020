5deb6a364b2d1e1a9a802d7805a280c0bec76e52
Pangjing-Wu <pangjing.wu@outlook.com>
2020-09-20 03:18:08 -0400

    add canny-hough

--- task3/canny.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-for f in range(1, 101):
-    file = './data/highway-snapshot/original_frame%s.bmp' % f
-    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    img = cv2.GaussianBlur(img,(5,5),0)
-    threshold = (10, 40)
-    cv2.imwrite('./fig/task3-canny/original_frame%s-canny.bmp' % f, cv2.Canny(img, *threshold))
